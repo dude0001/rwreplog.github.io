3de7b96f07dd56d5ce36fe8fcea5ee6834d59201
Ryan Replogle <ryan.replogle@gmail.com>
2024-09-13 21:08:54 -0400

    updates

--- data/index.ts
+++ data/index.ts
@@ -2,7 +2,7 @@ export const navItems = [
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
   { name: "Testimonials", link: "#testimonials" },
-  { name: "Contact", link: "#contact" },
+  { name: "Contact", link: "#contact" }
 ];
 
 export const gridItems = [
@@ -14,7 +14,7 @@ export const gridItems = [
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
     img: "/b1.svg",
-    spareImg: "",
+    spareImg: ""
   },
   {
     id: 2,
@@ -24,7 +24,7 @@ export const gridItems = [
     imgClassName: "",
     titleClassName: "justify-start",
     img: "",
-    spareImg: "",
+    spareImg: ""
   },
   {
     id: 3,
@@ -34,7 +34,7 @@ export const gridItems = [
     imgClassName: "",
     titleClassName: "justify-center",
     img: "",
-    spareImg: "",
+    spareImg: ""
   },
   {
     id: 4,
@@ -44,18 +44,18 @@ export const gridItems = [
     imgClassName: "",
     titleClassName: "justify-start",
     img: "/grid.svg",
-    spareImg: "/b4.svg",
+    spareImg: "/b4.svg"
   },
 
   {
     id: 5,
-    title: "Currently building a JS Animation library",
+    title: "Currently building an Angular DuckDB application",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
     img: "/b5.svg",
-    spareImg: "/grid.svg",
+    spareImg: "/grid.svg"
   },
   {
     id: 6,
@@ -65,8 +65,8 @@ export const gridItems = [
     imgClassName: "",
     titleClassName: "justify-center md:max-w-full max-w-60 text-center",
     img: "",
-    spareImg: "",
-  },
+    spareImg: ""
+  }
 ];
 
 export const projects = [
@@ -76,7 +76,7 @@ export const projects = [
     des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
     img: "/p1.svg",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "https://github.com/adrianhajdin?tab=repositories",
+    link: "https://github.com/adrianhajdin?tab=repositories"
   },
   {
     id: 2,
@@ -84,7 +84,7 @@ export const projects = [
     des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
     img: "/p2.svg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "https://github.com/adrianhajdin/zoom-clone",
+    link: "https://github.com/adrianhajdin/zoom-clone"
   },
   {
     id: 3,
@@ -92,7 +92,7 @@ export const projects = [
     des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
     img: "/p3.svg",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "https://github.com/adrianhajdin/ai_saas_app",
+    link: "https://github.com/adrianhajdin/ai_saas_app"
   },
   {
     id: 4,
@@ -100,8 +100,8 @@ export const projects = [
     des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
     img: "/p4.svg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "https://github.com/adrianhajdin/iphone",
-  },
+    link: "https://github.com/adrianhajdin/iphone"
+  }
 ];
 
 export const testimonials = [
@@ -109,32 +109,32 @@ export const testimonials = [
     quote:
       "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
     name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
+    title: "Director of AlphaStream Technologies"
   },
   {
     quote:
       "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
     name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
+    title: "Director of AlphaStream Technologies"
   },
   {
     quote:
       "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
     name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
+    title: "Director of AlphaStream Technologies"
   },
   {
     quote:
       "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
     name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
+    title: "Director of AlphaStream Technologies"
   },
   {
     quote:
       "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
     name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
+    title: "Director of AlphaStream Technologies"
+  }
 ];
 
 export const companies = [
@@ -142,32 +142,32 @@ export const companies = [
     id: 1,
     name: "cloudinary",
     img: "/cloud.svg",
-    nameImg: "/cloudName.svg",
+    nameImg: "/cloudName.svg"
   },
   {
     id: 2,
     name: "appwrite",
     img: "/app.svg",
-    nameImg: "/appName.svg",
+    nameImg: "/appName.svg"
   },
   {
     id: 3,
     name: "HOSTINGER",
     img: "/host.svg",
-    nameImg: "/hostName.svg",
+    nameImg: "/hostName.svg"
   },
   {
     id: 4,
     name: "stream",
     img: "/s.svg",
-    nameImg: "/streamName.svg",
+    nameImg: "/streamName.svg"
   },
   {
     id: 5,
     name: "docker.",
     img: "/dock.svg",
-    nameImg: "/dockerName.svg",
-  },
+    nameImg: "/dockerName.svg"
+  }
 ];
 
 export const workExperience = [
@@ -176,42 +176,42 @@ export const workExperience = [
     title: "Frontend Engineer Intern",
     desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
     className: "md:col-span-2",
-    thumbnail: "/exp1.svg",
+    thumbnail: "/exp1.svg"
   },
   {
     id: 2,
     title: "Mobile App Dev - JSM Tech",
     desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
     className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp2.svg",
+    thumbnail: "/exp2.svg"
   },
   {
     id: 3,
     title: "Freelance App Dev Project",
     desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
     className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp3.svg",
+    thumbnail: "/exp3.svg"
   },
   {
     id: 4,
     title: "Lead Frontend Developer",
     desc: "Developed and maintained user-facing features using modern frontend technologies.",
     className: "md:col-span-2",
-    thumbnail: "/exp4.svg",
-  },
+    thumbnail: "/exp4.svg"
+  }
 ];
 
 export const socialMedia = [
   {
     id: 1,
-    img: "/git.svg",
+    img: "/git.svg"
   },
   {
     id: 2,
-    img: "/twit.svg",
+    img: "/twit.svg"
   },
   {
     id: 3,
-    img: "/link.svg",
-  },
+    img: "/link.svg"
+  }
 ];
